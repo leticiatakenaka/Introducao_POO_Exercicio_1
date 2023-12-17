b9f20c15bfe95c325fa167a839d860296674a215
Leticia Santos Takenaka <leticiatakenaka12@gmail.com>
2023-12-17 18:51:52 -0300

    Adicionar arquivos de projeto.

---
+++ Introducao_POO_Exercicio_1/Pessoa.cs
@@ -0,0 +1,8 @@
+﻿namespace Introducao_POO_Exercicio_1
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+    }
+}
---
+++ Introducao_POO_Exercicio_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using Introducao_POO_Exercicio_1;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Maria";
+p1.Idade = 17;
+
+Pessoa p2 = new Pessoa();
+p2.Nome = "João";
+p2.Idade = 16;
+
+Console.WriteLine($"Dados pessoa 1: Nome: {p1.Nome} - Idade: {p1.Idade}");
+Console.WriteLine($"Dados pessoa 2: Nome: {p2.Nome} - Idade: {p2.Idade}");
